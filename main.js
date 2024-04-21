b2359c34801980132087fcf504429b5480b97e0e
abdullohnajmiddinov <abdullohnajmiddinov45@gmail.com>
2024-04-21 15:06:10 +0300

    h

---
+++ main.js
@@ -0,0 +1,27 @@
+let hafta =prompt('Birdan 7 gacha son kiriting')
+switch (hafta) {
+
+    case '1' :alert("Dushanba")
+      break;
+
+      case '2':alert("Seshanba")
+      break;
+
+      case '3':alert("Chorshanba")
+      break;
+
+      case '4':alert("Payshanba")
+      break;
+
+      case '5':alert("Juma")
+      break;
+
+      case '6':alert("Shanba")
+      break;
+
+      case '7':alert("Yakshanba")
+      break;
+
+      default:alert('Bir haftada 7 kun bor')
+      break;
+  }
